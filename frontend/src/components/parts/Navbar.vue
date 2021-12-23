92211ed431c0985e032c764b04fd570a76e8f6e1
ermalabiti <ermal.abiti@gmail.com>
2021-12-23 08:52:52 +0100

    add Login component

--- frontend/src/components/parts/Navbar.vue
+++ frontend/src/components/parts/Navbar.vue
@@ -1,37 +1,39 @@
 <template>
   <nav class="navbar is-dark">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/#/">
-      Tigrat e Zi
-    </a>
-  </div>
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/#/">
+          Forum App
+        </a>
+      </div>
 
-  <div id="navbarExampleTransparentExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="/#/">
-        Posts
-      </a>
-    </div>
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="/#/">
+            Posts
+          </a>
+        </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <a class="bd-tw-button button">
-              
-              <span>
-                Register
-              </span>
-            </a>
-          </p>
-          <p class="control">
-            <a class="button is-primary" >
-              <span>Login</span>
-            </a>
-          </p>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="bd-tw-button button">
+                  
+                  <span>
+                    Register
+                  </span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-primary" >
+                  <span>Login</span>
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   </div>
 </nav>
 </template>
