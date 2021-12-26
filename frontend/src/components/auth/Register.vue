f9585292473a753da80813e6804fcbc82b33489c
ermalabiti <ea47138@ubt-uni.net>
2021-12-26 19:08:13 +0100

    Update Register.vue

--- frontend/src/components/auth/Register.vue
+++ frontend/src/components/auth/Register.vue
@@ -1,59 +1,67 @@
 <template>
     <div class="container mt-5">
-        <h1 class="title has-text-centered">Register</h1>
         <div class="columns mt-5">
             <div class="column is-one-third"></div>
             <div class="column is-one-third">
-                <form class="form-horizontal" @submit="loginMethod">
-                    <fieldset>
+                <div class="card">
+                    <div class="card-content title">
+                        <p class="title">Not a member ?</p>
+                        <p class="subtitle">Join the largest community for discussions...</p>
+                    </div>
+                    <div class="card-content">
 
-                    <!-- Form Name -->
-                    <legend></legend>
+                        <form class="form-horizontal" @submit="loginMethod">
+                            <fieldset>
 
-                    <!-- Text input-->
-                    <div class="field">
-                    <label class="label" for="username">Username</label>
-                    <div class="control">
-                        <input id="username" name="username" type="text" placeholder="Username" class="input " required="">
-                        
-                    </div>
-                    </div>
+                            <!-- Form Name -->
+                            <legend></legend>
 
-                    <div class="field">
-                    <label class="label" for="username">Full Name</label>
-                    <div class="control">
-                        <input id="fullName" name="fullName" type="text" placeholder="Full Name" class="input " required="">
-                        
-                    </div>
-                    </div>
+                            <!-- Text input-->
+                            <div class="field">
+                            <label class="label" for="username">Username</label>
+                            <div class="control">
+                                <input id="username" name="username" type="text" placeholder="Username" class="input " required="">
+                                
+                            </div>
+                            </div>
 
-                    <div class="field">
-                    <label class="label" for="username">Email</label>
-                    <div class="control">
-                        <input id="email" name="email" type="text" placeholder="Email" class="input " required="">
-                        
-                    </div>
-                    </div>
+                            <div class="field">
+                            <label class="label" for="username">Full Name</label>
+                            <div class="control">
+                                <input id="fullName" name="fullName" type="text" placeholder="Full Name" class="input " required="">
+                                
+                            </div>
+                            </div>
 
-                    <!-- Password input-->
-                    <div class="field">
-                    <label class="label" for="password">Password</label>
-                    <div class="control">
-                        <input id="password" name="password" type="password" placeholder="Password" class="input " required="">
-                        
-                    </div>
-                    </div>
+                            <div class="field">
+                            <label class="label" for="username">Email</label>
+                            <div class="control">
+                                <input id="email" name="email" type="text" placeholder="Email" class="input " required="">
+                                
+                            </div>
+                            </div>
 
-                    <!-- Button -->
-                    <div class="field">
-                    <label class="label" for="registerbtn"></label>
-                    <div class="control">
-                        <button id="registerbtn" name="registerbtn" class="button is-success">Register</button>
-                    </div>
-                    </div>
+                            <!-- Password input-->
+                            <div class="field">
+                            <label class="label" for="password">Password</label>
+                            <div class="control">
+                                <input id="password" name="password" type="password" placeholder="Password" class="input " required="">
+                                
+                            </div>
+                            </div>
 
-                    </fieldset>
-                </form>
+                            <!-- Button -->
+                            <div class="field mt-5">
+                            <label class="label" for="registerbtn"></label>
+                            <div class="control">
+                                <button id="registerbtn" name="registerbtn" class="button is-fullwidth is-outlined is-success">Register</button>
+                            </div>
+                            </div>
+
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
             </div>
             <div class="column is-one-third"></div>
         </div>
