830d881e73c58de98809fb34911c868338baf64f
Eroll Uka <erollfaceit@gmail.com>
2022-03-08 13:05:06 +0100

    excluded password from GET method

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,7 +17,8 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     dateCreated: {
         type: Date,
