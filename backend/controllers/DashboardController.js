1c3cc3250f2847f3479dde65976c19533bd7a4dc
Leon <73724759+matrixleo@users.noreply.github.com>
2022-03-08 02:49:53 +0100

    Add getUsers

--- backend/controllers/DashboardController.js
+++ backend/controllers/DashboardController.js
@@ -1,3 +1,14 @@
+import User from "../models/User.js";
+
+
+//get ALL USERS, getfullName, getEmail, getUsername, getRole, getFollowers, getFollowing, 
 export const getUsers = async (req, res, next) => {
-    res.json({message: 'hell;oaosjdas'});
+    try {
+        const result = await User.find({});
+        return res.status(200).json(result);
+    }
+    catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
 }
+
--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
 import checkAdmin from "../middleware/checkAdmin.js";
-import { getUsers, } from "../controllers/DashboardController.js";
+import  { getUsers }  from "../controllers/DashboardController.js";
 
 
 const router = Router();
 // get /users
+
 // put /user ? userid=sl;akdsa;ld
+
+
 // delete /user ? user
 router.get('/users', getUsers);
 
